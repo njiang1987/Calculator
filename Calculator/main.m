bd86b2fd7c0b6ebd52a1a3fbd1e5a5befd1439be
njiang1987 <njiang1987@gmail.com>
2014-12-03 20:15:02 +0800

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by jn11585852 on 12/3/14.
+//  Copyright (c) 2014 JiangNan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
