9b30b249f7a06abec82148875115365c56706d38
njiang1987 <njiang1987@gmail.com>
2014-12-05 09:59:15 +0800

    test object

--- Calculator/AppDelegate.m
+++ Calculator/AppDelegate.m
@@ -8,8 +8,11 @@
 
 #import "AppDelegate.h"
 #import "CalculatorWindow.h"
+#import "MasterViewController.h"
 
 @interface AppDelegate ()
+
+@property (nonatomic,strong) IBOutlet MasterViewController *masterViewController;
 
 @property (weak) IBOutlet CalculatorWindow *window;
 - (IBAction)saveAction:(id)sender;
@@ -20,11 +23,19 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    // 1. Create the master View Controller
+    self.masterViewController = [[MasterViewController alloc] initWithNibName:@"MasterViewController" bundle:nil];
+    
+    // 2. Add the view controller to the Window's content view
+    [self.window.contentView addSubview:self.masterViewController.view];
+    self.masterViewController.view.frame = ((NSView*)self.window.contentView).bounds;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+
 
 #pragma mark - Core Data stack
 
---
+++ Calculator/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Calculator
+//
+//  Created by Jiang Nan on 12/4/14.
+//  Copyright (c) 2014 JiangNan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
